b58610265c6e78334b7a1340bd60eca2cda83867
HugoLito <110066926+Evout01@users.noreply.github.com>
2024-01-13 21:13:03 -0300

    Exercícios de HTML
    Exercícios realizados com o curso de JavaScript do Curso em Vídeo

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 80
+
+if (vel > 100) {
+    console.log('Você está acima da velocidade, multado!')
+}
+else {
+    console.log(`Sua velocidade é de ${vel} Velocidade ok! Dirija com cuidado.`)
+}
