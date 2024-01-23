1c80a6b471e5c5346ef983c788654be822fecfdd
Victor Hugo <victorferrarihugo@hotmail.com>
2024-01-22 22:02:30 -0300

    Alterações

--- Exercício 16/script.js
+++ Exercício 16/script.js
@@ -2,4 +2,9 @@ function contar(){
     var n1 = window.document.getElementById('inicio')
     var n2 = window.document.getElementById('fim')
     var passo = window.document.getElementById('passo')
+
+   console.log(n1)
+   console.log(n2)
+   console.log(passo)
+
 }
