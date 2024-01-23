299b4cb270c29e0f73be8fd849e02e8eab8e290c
Victor Hugo <victorferrarihugo@hotmail.com>
2024-01-22 21:49:57 -0300

    Exercício 16

---
+++ Exercício 16/script.js
@@ -0,0 +1,5 @@
+function contar(){
+    var n1 = window.document.getElementById('inicio')
+    var n2 = window.document.getElementById('fim')
+    var passo = window.document.getElementById('passo')
+}
