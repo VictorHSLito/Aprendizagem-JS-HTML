c8f9e521951687e97135cc575e5b510d2d7b3cc2
Victor Hugo <110066926+VictorHSLito@users.noreply.github.com>
2024-02-02 10:11:07 -0300

    Finalização Exercício 18

--- Exercício 18/script.js
+++ Exercício 18/script.js
@@ -1,11 +1,10 @@
 var num = document.getElementById('txtnum')
-var a = num.value
 var lista = document.getElementById('numlista')
 var result = document.getElementById('resultado')
 var valores = []
 
 
-function isNumero() {
+function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
         return true
     }
@@ -14,8 +13,8 @@ function isNumero() {
     }
 }
 
-function inLista() {
-    if(l.indexOf(Number(n)) != -1) {
+function inLista(n, l) {
+    if(Array.isArray(l) && l.indexOf(Number(n)) != -1) {
         return true
     }
     else {
@@ -25,9 +24,46 @@ function inLista() {
 
 function adiciona () {
     if(isNumero(num.value) && !inLista(num.value)) {
-        alert("Tudo funcionando")
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        resultado.innerHTML = ''
     }
     else {
         alert("Valor inválido ou já na está incluso na lista!")
     }
+    num.value = ''
+    num.focus()
+        
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
     }
+    else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }            
+        }
+        media = soma/tot
+
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.</p>`
+        resultado.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
+        resultado.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
+        resultado.innerHTML += `<p> Somando todos os valores temos ${soma}.</p>`
+        resultado.innerHTML += `<p> A média de todos os valores é ${media}.</p>`
+    }
+}
